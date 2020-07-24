ee80163c53751d6766a9ff7119e8434c6b92c173
binmaulin <67845804+binmaulin@users.noreply.github.com>
2020-07-24 21:56:43 +0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,15 +1,29 @@
-window.onload = function() {
-  var a = document.getElementById("html");
-  var scroll_width = 10; //滚动一下的距离
-  if(document.addEventListener){
-    document.addEventListener('DOMMouseScroll', mousewheel_event, false); // FF
-  }
-  a.onmousewheel = mousewheel_event; // IE/Opera/Chrome
-  function mousewheel_event(e) {
-    var e = e || window.event, v;
-    e.wheelDelta ? v=e.wheelDelta : v=e.detail;
-    if(v>3||-v>3) v=-v;
-    v>0 ? a.scrollLeft+=scroll_width : a.scrollLeft-=scroll_width;     
-    e.preventDefault(); //阻止浏览器的默认滚动
-  }
-};
+//判断浏览器
+let isIE = navigator.userAgent.match(/MSIE (\d)/i);
+isIE = isIE ? isIE[1] : undefined;
+let isFF = /FireFox/i.test(navigator.userAgent);
+if (isIE < 9) //传统浏览器使用MouseWheel事件
+  html.attachEvent("onmousewheel", function (e) {
+  //计算鼠标滚轮滚动的距离
+  //一格3行，每行40像素
+  let v = e.wheelDelta / 2;
+  html.scrollLeft += v;
+  //阻止浏览器默认方法
+  return false;
+});
+else if (!isFF) //除火狐外的现代浏览器也使用MouseWheel事件
+  html.addEventListener("mousewheel", function (e) {
+  //计算鼠标滚轮滚动的距离
+  let v = -e.wheelDelta / 2;
+  html.scrollLeft += v;
+  //阻止浏览器默认方法
+  e.preventDefault();
+}, false);
+else //火狐使用DOMMouseScroll事件
+  html.addEventListener("DOMMouseScroll", function (e) {
+  //计算鼠标滚轮滚动的距离
+  //一格是3行，但是要注意，这里和像素不同的是它是负值
+  html.scrollLeft += e.detail * 80;
+  //阻止浏览器默认方法
+  e.preventDefault();
+}, false);
