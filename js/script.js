a3bcc1e5c30c8fc46c276958af317584e92b556f
binmaulin <67845804+binmaulin@users.noreply.github.com>
2020-07-26 16:12:48 +0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ let isIE = navigator.userAgent.match(/MSIE (\d)/i);
 isIE = isIE ? isIE[1] : undefined;
 let isFF = /FireFox/i.test(navigator.userAgent);
 if (isIE < 9) //传统浏览器使用MouseWheel事件
-  html.attachEvent("onmousewheel", function (e) {
+  attachEvent("onmousewheel", function (e) {
   //计算鼠标滚轮滚动的距离
   //一格3行，每行40像素
   let v = e.wheelDelta / 2;
@@ -12,7 +12,7 @@ if (isIE < 9) //传统浏览器使用MouseWheel事件
   return false;
 });
 else if (!isFF) //除火狐外的现代浏览器也使用MouseWheel事件
-  html.addEventListener("mousewheel", function (e) {
+  addEventListener("mousewheel", function (e) {
   //计算鼠标滚轮滚动的距离
   let v = -e.wheelDelta / 2;
   html.scrollLeft += v;
@@ -20,7 +20,7 @@ else if (!isFF) //除火狐外的现代浏览器也使用MouseWheel事件
   e.preventDefault();
 }, false);
 else //火狐使用DOMMouseScroll事件
-  html.addEventListener("DOMMouseScroll", function (e) {
+  addEventListener("DOMMouseScroll", function (e) {
   //计算鼠标滚轮滚动的距离
   //一格是3行，但是要注意，这里和像素不同的是它是负值
   html.scrollLeft += e.detail * 80;
