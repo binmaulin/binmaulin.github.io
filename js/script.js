8383201249f4aefdb83d4732511d4ffcd489d19e
binmaulin <67845804+binmaulin@users.noreply.github.com>
2020-07-26 16:17:54 +0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -7,7 +7,7 @@ if (isIE < 9) //传统浏览器使用MouseWheel事件
   //计算鼠标滚轮滚动的距离
   //一格3行，每行40像素
   let v = e.wheelDelta / 2;
-  html.scrollLeft += v;
+  scrollLeft += v;
   //阻止浏览器默认方法
   return false;
 });
@@ -15,7 +15,7 @@ else if (!isFF) //除火狐外的现代浏览器也使用MouseWheel事件
   addEventListener("mousewheel", function (e) {
   //计算鼠标滚轮滚动的距离
   let v = -e.wheelDelta / 2;
-  html.scrollLeft += v;
+  scrollLeft += v;
   //阻止浏览器默认方法
   e.preventDefault();
 }, false);
@@ -23,7 +23,7 @@ else //火狐使用DOMMouseScroll事件
   addEventListener("DOMMouseScroll", function (e) {
   //计算鼠标滚轮滚动的距离
   //一格是3行，但是要注意，这里和像素不同的是它是负值
-  html.scrollLeft += e.detail * 80;
+  scrollLeft += e.detail * 80;
   //阻止浏览器默认方法
   e.preventDefault();
 }, false);
